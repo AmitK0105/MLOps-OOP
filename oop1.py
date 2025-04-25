c2c288423f5fdc9e01d6d63c89e225728ea7d18f
Amit Kumar <amitk.pr15@gmail.com>
2025-04-25 12:09:42 +0530

    Class and object basic done

---
+++ oop1.py
@@ -0,0 +1,20 @@
+#initiate a class
+class employee:
+    #special function/special method/ dunder method- constructer 
+    def __init__(self):
+        print("Started executed attributes or data")
+        self.id=123
+        self.salary= 50000
+        self.designation="SDE"
+        print("Attributes or data have been initiated")
+
+    def travel(self, destination):
+        print("This travel method was called manually")
+        print("Employee is now traveling to", destination)
+
+#Create the instatance or object of the class
+sam= employee()
+#print(sam.salary)
+#sam.travel("kerala")
+print(type(sam))
+
---
+++ rough.py
@@ -0,0 +1,7 @@
+list1= [1,3,4,19]
+my_str= "mlops playlist"
+my_int= 155
+print(type(list1))
+print(type(my_int))
+my_str= my_str.capitalize()
+print(my_str)
