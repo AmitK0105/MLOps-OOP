103f72cf2520afc17464826ac7ea3c44a39e0e4c
Amit Kumar <amitk.pr15@gmail.com>
2025-04-25 14:17:55 +0530

    covered till chatbook project

--- oop_project.py
+++ oop_project.py
@@ -11,15 +11,16 @@ class chatbook:
                           2. Press 2 for Signin
                           3. Press 3 for write a post
                           4. Press 4 for message a friend
-                          5. Press any other key for exit """)
+                          5. Press any other key for exit 
+                              ---> """)
         if user_input =="1":
             self.signup()
         elif user_input =="2":
             self.signin()
         elif user_input =="3":
-            pass
+            self.my_post()
         elif user_input =="4":
-            pass
+            self.send_message()
         else:
             exit()
 
@@ -46,6 +47,26 @@ class chatbook:
             else:
                 print("Please input correct credentials")
 
+        print("\n")
+        self.menu()
+
+    def my_post(self):
+        if self.loggedin==True:
+            text= input("Enter your message here -->")
+            print(f"following content has been posted -->", (text))
+        else:
+            print("You need to first signin to post something here-->")
+        print("\n")
+        self.menu()
+
+    def send_message(self):
+        if self.loggedin==True:
+            text1= input("Enter your message here -->")
+            frnd= input("Whom do you want to send this message ?")
+            print(f"Your message has sent to", (frnd))
+
+        else:
+            print("You need to signin first to post anything..")
         print("\n")
         self.menu()
 
--- rough.py
+++ rough.py
@@ -1,7 +1,10 @@
 list1= [1,3,4,19]
 my_str= "mlops playlist"
 my_int= 155
-print(type(list1))
-print(type(my_int))
-my_str= my_str.capitalize()
-print(my_str)
+#print(type(list1))
+#print(type(my_int))
+#my_str= my_str.capitalize()
+#print(my_str)
+
+from oop_project import chatbook
+user1=chatbook()
